323e7a346972c582db7690edab74aec50ca10fc7
Meus <dm.me.dev@outlook.com>
2024-11-24 09:07:50 +0700

    Rename and update main form properties

--- GDBox/Main.Designer.cs
+++ GDBox/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GDBox
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Main
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(784, 461);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "GDBox";
+            ResumeLayout(false);
         }
 
         #endregion
